b9cfb7d3f04a57ef9402ea2bf01dfcf29d3e81a4
kuchayaashiq <kucchayaashiq@gmail.com>
2023-05-31 10:29:23 +0530

    login and signup added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,11 @@ import React from "react";
 import logo from "./textlogo.svg";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 // import About from "./About";
 export default function Navbar(props) {
+  const { loginWithRedirect, logout } = useAuth0();
+  const { user, isAuthenticated } = useAuth0();
   return (
     <nav
       className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
@@ -42,76 +45,135 @@ export default function Navbar(props) {
               </Link>
             </li>
           </ul>
-          {/* <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form> */}
-          {/* <div className="gcse-search"></div> google search engine */}
-          <div className="d-flex">
-            <button
-              className="bg-primary rounded mx-2"
-              style={{ height: "20px", width: "20px", cursor: "pointer" }}
-              onClick={() => {
-                props.toggleMode("primary");
-              }}
-            ></button>
-            <button
-              className="bg-success rounded mx-2"
-              style={{ height: "20px", width: "20px", cursor: "pointer" }}
-              onClick={() => {
-                props.toggleMode("success");
-              }}
-            ></button>
-            <button
-              className="bg-danger rounded mx-2"
-              style={{ height: "20px", width: "20px", cursor: "pointer" }}
-              onClick={() => {
-                props.toggleMode("danger");
-              }}
-            ></button>
-            <button
-              className="bg-warning rounded mx-2"
-              style={{ height: "20px", width: "20px", cursor: "pointer" }}
-              onClick={() => {
-                props.toggleMode("warning");
-              }}
-            ></button>
-            <button
-              className="bg-light rounded mx-2"
-              style={{ height: "20px", width: "20px", cursor: "pointer" }}
-              onClick={() => {
-                props.toggleMode("light");
-              }}
-            ></button>
-            <button
-              className="bg-dark rounded mx-2"
-              style={{ height: "20px", width: "20px", cursor: "pointer" }}
-              onClick={() => {
-                props.toggleMode("dark");
-              }}
-            ></button>
-            <button
-              className="bg-info rounded mx-2"
-              style={{ height: "20px", width: "20px", cursor: "pointer" }}
-              onClick={() => {
-                props.toggleMode("info");
-              }}
-            ></button>
-            <label
-              className={`form-check-label text-${
-                props.mode === "dark" ? "light" : "dark"
-              }`}
-              htmlFor="flexSwitchCheckDefault"
-            >
-              Toggle Mode
-            </label>
+          <div className="d-flex ms-auto order-5 mt-2">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 pt-1">
+              {isAuthenticated && (
+                <li className="nav-item">
+                  {" "}
+                  <p className="">{user.name}</p>
+                </li>
+              )}
+              {isAuthenticated ? (
+                <li className="nav-item">
+                  <button
+                    className="  btn btn-primary btn-sm mx-3"
+                    onClick={() =>
+                      logout({
+                        logoutParams: { returnTo: window.location.origin },
+                      })
+                    }
+                  >
+                    Log Out
+                  </button>
+                </li>
+              ) : (
+                <li className="nav-item">
+                  <button
+                    className=" btn btn-primary btn-sm mx-3"
+                    onClick={() => loginWithRedirect()}
+                  >
+                    Log In
+                  </button>
+                </li>
+              )}
+            </ul>
+            <div class="dropdown pt-1">
+              <button
+                class="btn btn-secondary btn-sm dropdown-toggle mode"
+                type="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Mode
+              </button>
+              <ul class="dropdown-menu me-2">
+                <li className="d-flex align-items-center my-2 ">
+                  <button
+                    className=" dropdown-item bg-light rounded mx-2"
+                    style={{
+                      height: "20px",
+                      width: "20px",
+                      cursor: "pointer",
+                    }}
+                    onClick={() => {
+                      props.toggleMode("light");
+                    }}
+                  ></button>
+                  <label>light</label>
+                </li>
+                <li className="d-flex align-items-center my-2 ">
+                  <button
+                    className=" dropdown-item bg-dark rounded mx-2 "
+                    style={{
+                      height: "20px",
+                      width: "20px",
+                      cursor: "pointer",
+                    }}
+                    onClick={() => {
+                      props.toggleMode("dark");
+                    }}
+                  ></button>
+                  <label>dark</label>
+                </li>
+                {/* <li>
+                {" "}
+                <button
+                  className=" dropdown-item bg-primary rounded mx-2 "
+                  style={{ height: "20px", width: "20px", cursor: "pointer" }}
+                  onClick={() => {
+                    props.toggleMode("primary");
+                  }}
+                >
+                  <label className=" m-3 my-2">primary</label>
+                </button>
+              </li>
+              <li>
+                <button
+                  className=" dropdown-item bg-success rounded mx-2"
+                  style={{ height: "20px", width: "20px", cursor: "pointer" }}
+                  onClick={() => {
+                    props.toggleMode("success");
+                  }}
+                >
+                  success
+                </button>
+              </li>
+              <li>
+                <button
+                  className=" dropdown-item bg-danger rounded mx-2"
+                  style={{ height: "20px", width: "20px", cursor: "pointer" }}
+                  onClick={() => {
+                    props.toggleMode("danger");
+                  }}
+                >
+                  danger
+                </button>
+              </li>
+              <li>
+                <button
+                  className=" dropdown-item bg-warning rounded mx-2"
+                  style={{ height: "20px", width: "20px", cursor: "pointer" }}
+                  onClick={() => {
+                    props.toggleMode("warning");
+                  }}
+                >
+                  warning
+                </button>
+              </li>
+
+              <li>
+                <button
+                  className=" dropdown-item bg-info rounded mx-2"
+                  style={{ height: "20px", width: "20px", cursor: "pointer" }}
+                  onClick={() => {
+                    props.toggleMode("info");
+                  }}
+                >
+                  info
+                </button>
+              </li> */}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-npgw0scomytf1t4y.us.auth0.com"
+    clientId="YuK7PPYw1tgPChe9ZbvAOMuZBHOySruJ"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
